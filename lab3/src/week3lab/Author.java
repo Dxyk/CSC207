765e41e5038810122075773029d8ca4484644bc0
Kong Xiangyu <kongxi16@cdf.toronto.edu>
2016-10-04 10:39:35 -0400

    finished Author

--- lab3/src/week3lab/Author.java
+++ lab3/src/week3lab/Author.java
@@ -1,5 +1,32 @@
 package week3lab;
 
 public class Author {
+	private String first_name;
+	private String last_name;
+	
+	public Author(String first_name, String last_name) {
+		this.first_name = first_name;
+		this.last_name = last_name;
+	}
+	
+	public String getFirstName() {
+		return this.first_name;
+	}
+	
+	public void setFirstName(String first_name) {
+		this.first_name = first_name;
+	}
+	
+	public String getLastName() {
+		return this.last_name;
+	}
+	
+	public void setLastName(String last_name) {
+		this.last_name = last_name;
+	}
 
+	public String toString() {
+		return this.first_name.toString() + ", " + this.last_name.toString();
+	} 
+	
 }
