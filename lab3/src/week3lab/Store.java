520e8f95164f1efc1f48d32eca0da0ce2f28929c
Daniel <danielkong1997@gmail.com>
2016-10-15 15:19:12 -0400

    finished Store in lab3

--- lab3/src/week3lab/Store.java
+++ lab3/src/week3lab/Store.java
@@ -4,7 +4,8 @@ public class Store {
 	public void main(String[] args) {
 		Author a1 = new Author("Russle", "Winder");
 		Author a2 = new Author("Graham", "Roberts");
-		Author[] authors = new Author[];
-		Book b = new Book("Developing Java Software", []);
+		Author[] authors = {a1, a2};
+		Book b = new Book("Developing Java Software", authors, "1234567", 1240);
+		System.out.println(b);
 	}
 }
