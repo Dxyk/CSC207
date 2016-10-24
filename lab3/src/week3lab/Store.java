77472972c5983e0acd086a917543a5faacbb4c19
Daniel <danielkong1997@gmail.com>
2016-10-24 15:30:09 -0400

    edited lab3

--- lab3/src/week3lab/Store.java
+++ lab3/src/week3lab/Store.java
@@ -2,9 +2,7 @@ package week3lab;
 
 public class Store {
 	public void main(String[] args) {
-		Author a1 = new Author("Russle", "Winder");
-		Author a2 = new Author("Graham", "Roberts");
-		Author[] authors = {a1, a2};
+		Author[] authors = {new Author("Russle", "Winder"), new Author("Graham", "Roberts")};
 		Book b = new Book("Developing Java Software", authors, "1234567", 1240);
 		System.out.println(b);
 	}
